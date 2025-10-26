e785ea6f2c9ebe35782357e31ec95dbac5ac6cb1
lilcharchar <jj453833@gmail.com>
2025-10-26 15:42:59 -0500

    conexión inicial backend y front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: "http://localhost:5173", // o 3000, depende de tu frontend
+    credentials: true,
+  });
+
+  await app.listen(5000);
+  console.log("🚀 Backend corriendo en http://localhost:5000");
 }
 void bootstrap();
